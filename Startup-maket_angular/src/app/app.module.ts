27bcd211508834e64dae2dfc5a4f1a62770271fb
ashyrovsasha18@icloud.com <ashyrovsasha18@icloud.com>
2023-03-28 22:37:31 +0500

    Сделал навигацию начал делать основной блок

--- Startup-maket_angular/src/app/app.module.ts
+++ Startup-maket_angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LeftsideBarComponent } from './leftside-bar/leftside-bar.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftsideBarComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ Startup-maket_angular/src/app/leftside-bar/leftside-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leftside-bar',
+  templateUrl: './leftside-bar.component.html',
+  styleUrls: ['./leftside-bar.component.sass']
+})
+export class LeftsideBarComponent {
+
+}
